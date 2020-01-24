aa98fc336012e48244bf4d72341884e851d457f4
Peter Poloskei <pepo@ipp.mpg.de>
2020-01-24 10:30:05 +0100

    #3 minor modification and testfile

--- python/bicoherence/bicoherence_calculations.py
+++ python/bicoherence/bicoherence_calculations.py
@@ -6,6 +6,8 @@ Created on Fri Dec  6 15:00:17 2019
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
+from scipy import signal as _sig
 
 def calculateBicoherence(signal, timeax, blocksize, phaserandomized = False, debug = False):
     """Calculate the bicoherence matrix for a given signal.
@@ -22,7 +24,7 @@ def calculateBicoherence(signal, timeax, blocksize, phaserandomized = False, deb
     freq -- frequency axis of the bicoherence matrix [blocksize/2]/int
     bicoherence_matrix -- 2D matrix containing bicoherence values [blocksize/2, blocksize/2]/double
     """
-    
+    fmax = 300
     n_signal = len(signal)
     n_block = int(n_signal/blocksize)
     
@@ -32,15 +34,24 @@ def calculateBicoherence(signal, timeax, blocksize, phaserandomized = False, deb
         print('Single block length / ms: ', '{:.2f}'.format((timeax[blocksize]-timeax[0])*1000))
         print('Number of blocks:', n_block)
     
-    #Cut signal into smaller pieces, cut edge, remove mean.
-    signal -= np.mean(signal)
-    signal = np.reshape(signal[0:n_block*blocksize], [n_block,blocksize])
-    timeax = timeax[0:blocksize]
+    #Cut signal into smaller pieces, cut edge, remove mean apply hanning window.
+    windowing ='hanning'
+    if windowing is 'hanning':
+        _signal = np.zeros((2*n_block-1, blocksize))
+        for i in range(2*n_block-1):
+            s = signal[i*int(blocksize/2):i*int(blocksize/2) + blocksize]
+            _signal[i,:] = np.hanning(blocksize)*(s-np.mean(s))
+        signal = _signal
+    else:
+        signal -= np.mean(signal)
+        signal = np.reshape(signal[0:n_block*blocksize], [n_block,blocksize])
+        timeax = timeax[0:blocksize]
     
     #Do FFT
     sp = np.fft.fft(signal)
     freq = np.fft.fftfreq(blocksize, d =(timeax[1]-timeax[0]))/1000. #Hz to kHz
     ind = np.where(freq >=0)
+    ind = np.where((freq >= 0) & (freq < fmax))
     freq = freq[ind]
     n_sp = len(freq)
     sp = sp[:,ind][:,0,:]
@@ -57,7 +68,7 @@ def calculateBicoherence(signal, timeax, blocksize, phaserandomized = False, deb
         
     shift_term = np.zeros((n_block, n_sp, n_sp), dtype = complex)
     for i in range(n_block):
-        for j in range(int(blocksize/2)):
+        for j in range(n_sp):
             shift_term[i,j,:] = np.roll(sp[i,:], -(j+1))
     bispec = np.mean(cross_term*np.conj(shift_term), axis = 0)
     norm1 = np.sqrt(np.mean(np.abs(cross_term**2),axis = 0))
@@ -66,30 +77,32 @@ def calculateBicoherence(signal, timeax, blocksize, phaserandomized = False, deb
     bicoherence_matrix = np.abs(bispec)/(norm1*norm2)
     
     if debug is True:
-        lo = 6 #kHz
-        hi = 10 #kHz
+        lo = 55 #kHz
+        hi = 63 #kHz
         ind = np.where((lo<freq) & (freq<hi))[0]
         fig = plt.figure()
         ax = fig.add_subplot(211)
         ax.plot(freq, np.mean(bicoherence_matrix[ind,:], axis = 0), color  = 'xkcd:black', label = str(lo)+' - '+str(hi)+' kHz')
+        ax.plot(freq, np.mean(np.abs(sp)**2, axis=0)/np.max(np.mean(np.abs(sp)**2, axis=0)), lw = 0.5)
         ax.set_xlabel('Frequency [kHz]')
         ax.set_ylabel('<b>')
         ax.legend()
         
         ax = fig.add_subplot(223)
-        ax.plot(np.mean(np.abs(sp)**2, axis=0)/160000, freq)
+        ax.plot(np.mean(np.abs(sp)**2, axis=0), freq)
         ax.set_xlabel('Amplitude [a.u.]')
         ax.set_ylabel('Frequency [kHz]')
         
         ax = fig.add_subplot(224)
-        cm = ax.pcolormesh(freq,freq, bicoherence_matrix**0.2, alpha = 1.)
-        ax.contour(freq,freq, bicoherence_matrix, colors = 'black', linewidths = 0.2)
+        cm = ax.pcolormesh(freq,freq, bicoherence_matrix**0.2, vmin = 0., vmax = 1., alpha = 1.)
+#        ax.contour(freq,freq, bicoherence_matrix, colors = 'black', linewidths = 0.2)
         cbar = plt.colorbar(cm)
         cbar.set_label('Bicoherence')
         ax.plot(freq,freq, 'k--')
         ax.plot(freq,-freq+np.max(freq), 'k--')
         ax.set_xlabel('Frequency [kHz]')
         ax.set_ylabel('Frequency [kHz]')
+        
         fig.tight_layout()
     return freq, bicoherence_matrix
 
@@ -108,14 +121,23 @@ def calculateBicoherenceSignificance(signal, timeax, blocksize, n_samples, debug
 def filterBicoherenceMatrix(freq, bicoherence, bicoherence_density, alpha, debug = False):
     dim = np.shape(bicoherence_density)
     filtered_bicoherence = np.zeros_like(bicoherence)
-    
+    p  = [0.68, 0.95, 0.997]
+    critical_bicoherence_0680 = np.zeros_like(bicoherence)
+    critical_bicoherence_0950 = np.zeros_like(bicoherence)
+    critical_bicoherence_0997 = np.zeros_like(bicoherence)
+    critical_bicoherence = np.zeros_like(bicoherence)
     for i in range(dim[1]):
         for j in range(dim[2]):
+            dens = bicoherence_density[:,i,j]
+            dens = np.sort(dens)
+            critical_bicoherence_0680[i,j] = dens[int(dim[0]*p[0])]
+            critical_bicoherence_0950[i,j] = dens[int(dim[0]*p[1])]
+            critical_bicoherence_0997[i,j] = dens[int(dim[0]*p[2])]
             count = len(np.where(bicoherence[i,j] > bicoherence_density[:,i,j])[0])
             if count/dim[0] > alpha:
                 filtered_bicoherence[i,j] = bicoherence[i,j]
     if debug is True:
-        cmap=plt.cm.get_cmap('plasma')
+        cmap=plt.cm.get_cmap('viridis')
         cmap.set_bad(color='white')
         masked_array = np.ma.masked_where(filtered_bicoherence == 0, filtered_bicoherence)
         
@@ -132,14 +154,29 @@ def filterBicoherenceMatrix(freq, bicoherence, bicoherence_density, alpha, debug
         
         ax = fig.add_subplot(122, sharex=ax, sharey=ax)
         cm = ax.pcolormesh(freq, freq, masked_array**0.2, alpha = 1., vmin = 0., vmax = 1., cmap = cmap)
-        ax.contour(freq, freq, filtered_bicoherence, colors = 'black', linewidths = 0.2)
+#        ax.contour(freq, freq, filtered_bicoherence, colors = 'black', linewidths = 0.2)
         cbar = plt.colorbar(cm)
         cbar.set_label('Filtered - bicoherence')
         ax.plot(freq,freq, 'k--')
         ax.plot(freq,-freq+np.max(freq), 'k--')
         ax.set_xlabel('Frequency [kHz]')
-#        ax.set_ylabel('Frequency [kHz]')
         fig.tight_layout()
+        
+        fig = plt.figure(figsize=(10,6))
+        ax = fig.add_subplot(111)
+        lo = 40 #kHz
+        hi = 63 #kHz
+        ind = np.where((lo<freq) & (freq<hi))[0]
+        ax.plot(freq, np.mean(bicoherence[ind,:], axis = 0), color = 'xkcd:black', label = str(lo)+' - '+str(hi)+' kHz')
+        ax.plot(freq, np.mean(critical_bicoherence_0680[ind,:], axis = 0), color = 'xkcd:yellow', lw = 1, alpha = 0.7, label = r'$\alpha$ = '+str(p[0]))
+        ax.plot(freq, np.mean(critical_bicoherence_0950[ind,:], axis = 0), color = 'xkcd:orange', lw = 1, alpha = 0.9, label = r'$\alpha$ = '+str(p[1]))
+        ax.plot(freq, np.mean(critical_bicoherence_0997[ind,:], axis = 0), color = 'xkcd:red', lw = 1, alpha = 1., label = r'$\alpha$ = '+str(p[2]))
+#        ax.set_ylim([0,1])
+#        ax.set_xlim([0,49.5])
+        ax.set_ylabel('<b> \ -')
+        ax.set_xlabel('Frequency \ kHz')
+        ax.grid('on')
+        ax.legend()
     return filtered_bicoherence
 
 def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -167,18 +204,61 @@ if __name__ == '__main__':
     # simple test cas for the runs
     plt.close('all')
     # setup a signal which shows bicoherence
-    n = 80000
-    t = np.linspace(0,0.8,n)
-    dt = t[1]-t[0]
-    A1 = 5
-    A2 = 5
-    w1 = 30e3
-    w2 = 38e3
-    w3 = 8e3
-    x = 10*np.random.rand(n) + A1*np.sin(2*np.pi*w1*t) + A2*np.sin(2*np.pi*w2*t) +  5*np.sin(2*np.pi*w3*t)
+#    n = 80000
+#    t = np.linspace(0,0.8,n)
+#    dt = t[1]-t[0]
+#    A1 = 5
+#    A2 = 5
+#    w1 = 30e3
+#    w2 = 38e3
+#    w3 = 8e3
+#    x = 10*np.random.rand(n) + A1*np.sin(2*np.pi*w1*t) + A2*np.sin(2*np.pi*w2*t) +  5*np.sin(2*np.pi*w3*t)
 #    print(1/dt/1000., ' kHz')
-    n = 200 # number of samples drawn from the local (f1,f2) random bicoherence density function
-    blocksize = 2**9
-    f, b = calculateBicoherence(x,t,blocksize,False,True)
+#    n = 50 # number of samples drawn from the local (f1,f2) random bicoherence density function
+#    blocksize = 2**9
+#    f, b = calculateBicoherence(x,t,blocksize,False,False)
 #    _f, b_dens = calculateBicoherenceSignificance(x,t,blocksize,n)
-    filtered_b = filterBicoherenceMatrix(f, b, b_dens, 0.995, True)
+#    filtered_b = filterBicoherenceMatrix(f, b, b_dens, 0.995, True)
+
+    magnetics = sp.io.readsav('./32388_b31-14_short.sav')
+    t = np.array(magnetics['time'])
+    x = np.array(magnetics['data'])
+    #testsignal
+#    A1 = 20
+#    A2 = 30
+#    A3 = 10
+#    w1 = 59e3
+#    w2 = 140e3
+#    x = 30*np.random.rand(len(t)) + A1*np.sin(2*np.pi*w1*t) + A2*np.sin(2*np.pi*w2*t) +  A3*np.sin(2*np.pi*(w1+w2)*t)
+#    
+    ind = np.where((t> 0.97) & (t<1.))
+
+    blocksize = 2**9
+    n = 100
+    f, b = calculateBicoherence(x[ind],t[ind],blocksize,False,True)
+##    
+    _f, b_dens = calculateBicoherenceSignificance(x[ind],t[ind],blocksize,n)
+    filtered_b = filterBicoherenceMatrix(f, b, b_dens, 0.9, True)
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111)
+#    ax.plot(t[ind], x[ind])
+    
+    fs = ((t[-1]-t[0])/(len(t)-1))**-1
+    std_dev = 2**7
+    dt = (t[std_dev]-t[0])*1e6
+    print('stft window length: [us]', '{:.2f}'.format(dt))     
+    length = std_dev*10
+    window = _sig.get_window(("gaussian", std_dev), length)
+    nperseg = length
+    noverlap =  int(nperseg/2.)
+    f, t, Sxx = _sig.spectrogram(x[ind], fs, window = window, nperseg = nperseg, noverlap = noverlap)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cm = ax.pcolormesh(t, f/1000, Sxx**0.2)
+    cbar = plt.colorbar(cm)
+    ax.axhline(40, color = 'white')
+    ax.axhline(63, color = 'white')
+#    ax.plot(t, np.ones(t), lw = 2)
+    ax.set_ylim([0,300])
+    ax.set_ylabel('Frequency [kHz]')
+    ax.set_xlabel('Time [sec]')
