9d99fcd3d8617f2159a831c136f418297026d8d0
Márton Vavrik <vavmarci2@gmail.com>
2020-08-31 22:28:02 +0200

    #26 minor changes to save and load

--- python/core/core.py
+++ python/core/core.py
@@ -3,7 +3,7 @@
 
 import sys
 
-import scipy.io
+from scipy.io import readsav
 import io
 import flap
 import logging
@@ -44,25 +44,34 @@ class NWTDataObject:
         return
 
     def update_properties(self):
-        self.raw_datapoints = self.raw_data.data.shape[-1]
+        if self.raw_data is not None:
+            self.raw_datapoints = self.raw_data.data.shape[-1]
+
+    def reset(self):
+        self.__init__(logger=self.logger)
 
     def load_proc_sav(self, path):
-        self.logger.info("Loading sav file: " + path)
-        loaded_sav = io.readsav(path, python_dict=True)
-        self.raw_data, self.transforms, self.smoothed_apsds, self.crosstransforms, self.smoothed_crosstransforms, \
-        self.coherences, self.transfers, self.modenumbers, self.qs = convert_dict_to_flap.convert_processed_sav(
-            loaded_sav)
-        self.update_properties()
+        try:
+            self.logger.info("Loading sav file: " + path)
+            loaded_sav = readsav(path, python_dict=True)
+            self.reset()
+            self.raw_data, self.transforms, self.smoothed_apsds, self.crosstransforms, self.smoothed_crosstransforms, \
+            self.coherences, self.transfers, self.modenumbers, self.qs = convert_dict_to_flap.convert_processed_sav(
+                loaded_sav)
+            self.update_properties()
+        except Exception as e:
+            self.logger.error("Exception occurred during processed sav loading:", exc_info=True)
         return
 
     def load_raw_sav(self, path):
         self.logger.info("Loading sav file: " + path)
-        loaded_sav = scipy.io.readsav(path, python_dict=True)
+        loaded_sav = readsav(path, python_dict=True)
         if "transf_timeax" in loaded_sav or "transf_freqax" in loaded_sav:
             self.logger.warning("Probably trying to load processed sav, please use 'Load processed' button")
+            return
         try:
-            flap_object = convert_dict_to_flap.convert_raw_sav(loaded_sav, skip_keys=[])
-            self.raw_data = flap_object
+            self.reset()
+            self.raw_data = convert_dict_to_flap.convert_raw_sav(loaded_sav, skip_keys=[])
             self.logger.debug("Loaded sav file")
         except TypeError('loaded_sav is not a dictionary of a raw sav file'):
             self.logger.error('loaded_sav is not a dictionary of a raw sav file', exc_info=True)
@@ -106,28 +115,20 @@ class NWTDataObject:
             return
 
     def save(self, path):
-        if path[-6:] == ".pynwt":
-            try:
-                f = io.open(filename, "rb")
-                loaded = pickle.dump(f)
-                f.close()
-
-                self.raw_data = loaded.raw_data
-                self.transforms = loaded.transforms
-                self.smoothed_apsds = loaded.smoothed_apsds
-                self.crosstransforms = loaded.crosstransforms
-                self.smoothed_crosstransforms = loaded.smoothed_crosstransforms
-                self.coherences = loaded.coherences
-                self.transfers = loaded.transfers
-                self.modenumbers = loaded.modenumbers
-                self.qs = loaded.qs
-                self.update_properties()
-                return
-            except Exception as e:
-                core_logger.error('Error during loading', exc_info=True)
-
-        else:
+        path = path + ".pynwt"
+        try:
+            filename = (path.split('/'))[-1]
+            # saved = NWTDataObject()
+            # saved.raw_data = self.raw_data
+            print(filename)
+            f = open(filename, "wb")
+            d = self.raw_data
+            pickle.dump(d, f)
+            f.close()
             return
+        except Exception as e:
+            core_logger.error('Error during loading', exc_info=True)
+
 
 
 def load_pynwt(path):
