50b6587d932b5ab7ea5ccc0ad197d3e91b77ccdf
Márton Vavrik <vavmarci2@gmail.com>
2021-07-26 22:36:53 +0200

    #40 ridge calculation added

--- python/utility/transform_error_esimation.py
+++ python/utility/transform_error_esimation.py
@@ -81,6 +81,59 @@ def distribution_region_fit_recursive_helper(data, fit_method, logger, params=[]
             tmp_params.append(distribution_region_fit_recursive_helper(np.abs(i), fit_method=fit_method, logger=logger))
         return tmp_params
 
+def get_ridge(stft, f_search_start=None, search_r=30, time_start=None, time_end=None, freq_start=None, freq_end=None,
+              coords=["Time", "Frequency"], logger=default_logger):
+    # returns a flap dataobject, with the maximum amplitudes
+    # the non-equidistant frequency axis contains the frequency location of the peaks
+
+    if len(stft.data.shape) != 2:
+        logger.error("Get ridge currently only works with 2D data (with one channel, so use slicing)", exc_info=True)
+
+    if freq_start is None and freq_end is None and time_start is None and time_end is None:
+        stft = stft
+        logger.debug('No slice, entire data will be used')
+    elif freq_start is None and freq_end is None:
+        stft = stft.slice_data(slicing={coords[0]: flap.Intervals(time_start, time_end)})
+        logger.debug('Only first (time) axis sliced')
+    elif time_start is None and time_end is None:
+        stft = stft.slice_data(slicing={coords[1]: flap.Intervals(freq_start, freq_end)})
+        logger.debug('Only second (frequency) axis sliced')
+    else:
+        stft = stft.slice_data(slicing={coords[0]: flap.Intervals(time_start, time_end),
+                                               coords[1]: flap.Intervals(freq_start, freq_end)})
+        logger.debug('Both axes sliced')
+
+    if stft.get_coordinate_object(coords[0]).dimension_list[0] != 1:
+        np.swapaxes(sliced.data, 1, 0)
+        stft.get_coordinate_object(coords[0]).dimension_list[0] = 1
+        stft.get_coordinate_object(coords[1]).dimension_list[0] = 0
+        #the stft should only have 2 dimensions, Frequency and Time (in order)
+    t_dim=1
+
+    if f_search_start == None:
+        f_search_start = np.argmax(np.abs(stft.data[:, 0]))
+    else:
+        i_search_start=stft.index_from_coordinate(coords[1],f_search_start)
+
+    ridgei = np.zeros(stft.data.shape[t_dim])
+    ridge_amp = np.zeros(stft.data.shape[t_dim], dtype=complex)
+    global_max = np.max(np.abs(stft.data))
+    print(global_max)
+
+    #search_area = np.abs(stft[f_search_start - include_r:f_search_start + include_r])
+    ridgei[0] = np.argmax(np.abs(stft.data[:, 0]))
+    ridge_amp[0] = stft.data[int(ridgei[0]), 0]
+    for i in range(1, stft.data.shape[1]):
+        i_min=max(ridgei[i-1]-search_r,0)
+        i_max=min(ridgei[i-1]+search_r,stft.data.shape[0])
+        ridgei[i] = np.argmax(np.abs(stft.data[int(i_min):int(i_max), i]))+ridgei[i-1]-search_r
+        if ridgei[i]<global_max/5:
+            print(i)
+            ridgei[i] = np.argmax(np.abs(stft.data[:, i]))
+        ridge_amp[i] = stft.data[int(ridgei[i]), i]
+
+    return ridgei, ridge_amp
+
 def rice_get_error_bars(shape,loc,scale):
     # scale is the sigma deviation of the 2D Gauss-distribution
     # returns the two ends of the error bar based on the appropriate percentile (+- 0.341)
